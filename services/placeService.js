1a78853fef6840be1ab5d963b9a5d2feafe7480e
sohyeong-dev <so-hyeong@naver.com>
2021-02-05 03:23:44 +0900

    fix: filter parameter value

--- services/placeService.js
+++ services/placeService.js
@@ -1,8 +1,15 @@
 import Place from '../models/place.js';
 
+const categories = {
+  'tree': '가로수길',
+  'history': '향토문화유적',
+  'forest': '휴양림',
+  'tour': '관광지',
+};
+
 export const getPlaces = async (position, category = null) => {
   const where = category ? {
-    category
+    category: categories[category],
   } : {};
   const places = await Place.find( {
     ...where,
